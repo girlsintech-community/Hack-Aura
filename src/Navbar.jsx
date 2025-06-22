976e96512443680256ca9bba84bcc9c08faad05c
kashikaga <kgupta.abps@gmail.com>
2025-06-22 21:22:40 +0530

    adding girls in tech logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,33 +15,34 @@ const Navbar = ({onContactClick}) => {
   }, []);
 
   return (
-    <div className="navbar-wrapper">
-      {/* Fixed Logo on the left */}
-      <div className="navbar-logo">
-        <a href="/">
-          <img src={logo} alt="HackAura Logo" />
-        </a>
-        <div className="navbar-logo">
-        <a href="/">
-          <img src={girlsintech} alt="GirlsInTech Logo" />
-        </a>
-        </div>
-      </div>
+   <div className="navbar-wrapper">
+  {/* Left Logo */}
+  <div className="navbar-logo">
+    <a href="/">
+      <img src={logo} alt="HackAura Logo" />
+    </a>
+  </div>
 
-      {/* Centered navbar */}
-      <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
-        <a href="/">Home</a>
-        <a href="#about">About</a>
-        <a href="#timeline">Timeline</a>
-        <a href="#sponsors">Sponsors</a>
-        <a href="/humans">Humans of HackAura</a>
-        <a href="#footer">Support</a>
-        <a onClick={onContactClick} style={{ cursor: 'pointer' }}>Contact</a>
-      </nav>
+  {/* Center Nav Links */}
+  <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+    <a href="/">Home</a>
+    <a href="#about">About</a>
+    <a href="#timeline">Timeline</a>
+    <a href="#sponsors">Sponsors</a>
+    <a href="/humans">Humans of HackAura</a>
+    <a href="#footer">Support</a>
+    <a onClick={onContactClick} style={{ cursor: 'pointer' }}>Contact</a>
+  </nav>
+
+  {/* Right GirlsInTech Icon */}
+  <div className="navbar-girls-icon">
+    <a href="/">
+      <img src={girlsintech} alt="GirlsInTech Logo" />
+    </a>
+  </div>
+</div>
 
 
-
-    </div>
   );
 };
 

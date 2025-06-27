199697e7c9be71c0cbb93d0adf1a5aa5de955868
LAPTOP-AE1EG6V2\HP <adyashadas0405@gmail.com>
2025-06-27 10:02:34 +0530

    Fixed the Footer Navigation

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -100,12 +100,12 @@ function Footer() {
         <div className="footer-right">
           <h3>Quick Links</h3>
           <ul className="quick-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#timeline">Timeline</a></li>
-            <li><a href="#sponsors">Sponsors</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about">About</a></li>
+            <li><a href="/#timeline">Timeline</a></li>
+            <li><a href="/#sponsors">Sponsors</a></li>
             <li><a href="/humans">Humans of HackAura</a></li>
-            <li><a href="#faq">FAQ</a></li>
+            <li><a href="/#faq">FAQ</a></li>
             <li><a href="/code-of-conduct">Code of Conduct</a></li>
           </ul>
         </div>

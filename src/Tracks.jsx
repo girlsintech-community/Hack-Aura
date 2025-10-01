b368c2c5d3baae4533c4e5322ec22bc24f9b6223
kashikaga <kgupta.abps@gmail.com>
2025-10-02 01:47:21 +0530

    adding problem statements and prizes

--- src/Tracks.jsx
+++ src/Tracks.jsx
@@ -16,17 +16,20 @@ const RollingGallery = () => {
     {
       title: 'AI',
       desc: 'Create smart, adaptive tools using neural networks and deep learning.',
-      prize: 'Prizes to be announced'
+      prize: 'AI',
+      link:'https://docs.google.com/document/d/1sQXHEzf3A5b7DyUKsqhRrXG8JAq-y-qEsoBLAK6iRSg/edit?usp=sharing'
     },
     {
       title: 'Blockchain',
       desc: 'Build transparent, trustless applications with secure smart contracts.',
-      prize: 'Prizes to be announced'
+      prize: 'Blockchain',
+      link:'https://docs.google.com/document/d/1tl7KIRRFEVYUo1tOTCCRKTTH5urKU8Qf3xSN2OLOd58/edit?tab=t.0'
     },
     {
       title: 'Open Innovation',
       desc: 'Collaborate and solve real-world problems through cross-discipline ideas.',
-      prize: 'Prizes to be announced'
+      prize: 'Open Innovation',
+      link:'https://docs.google.com/document/d/1h_Ul5vHD-hFxw0_CQ6j_4bPybG4C7BlF-1lLL4fUvp8/edit?tab=t.0'
     }
   ];
 
@@ -43,11 +46,18 @@ const RollingGallery = () => {
             <div className="card1-face card1-front">
               <h3 className="card1-title">{item.title}</h3>
               <p className="card1-desc">{item.desc}</p>
-              <div className="flip-hint">Click to see prizes</div>
+              <div className="flip-hint">Problem Statements and Prizes</div>
             </div>
             <div className="card1-face card1-back">
-              <h3 className="card1-title">{item.title} Prizes</h3>
-              <p className="card1-prize">{item.prize}</p>
+              <h3 className="card1-title">{item.prize}</h3> 
+               <a 
+                href={item.link} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="flip-btn"
+              >
+                Click here to View
+              </a>
               <div className="flip-hint">Click to return</div>
             </div>
           </div>
